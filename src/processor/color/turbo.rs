f67fb35c57f5f307124f7b3e6956b3aab90c7888
Aytixel <aytixel@gmail.com>
2026-04-05 14:25:06 +0200

    Add YCbCr support to turbojpeg

--- src/processor/color/turbo.rs
+++ src/processor/color/turbo.rs
@@ -1,78 +1,105 @@
 use std::error::Error;
 
-use turbojpeg::{decompress, PixelFormat};
+use turbojpeg::{yuv_pixels_len, Decompressor, Image, PixelFormat, YuvImage};
 
 use crate::processor::ProcessorTrait;
 
-use super::{ColorFrame, ColorSpace, ColorPacket};
+use super::{ColorFrame, ColorPacket, ColorSpace};
 
-impl From<PixelFormat> for ColorSpace {
-    fn from(value: PixelFormat) -> Self {
+impl From<Option<PixelFormat>> for ColorSpace {
+    fn from(value: Option<PixelFormat>) -> Self {
         match value {
-            PixelFormat::RGB => Self::RGB,
-            PixelFormat::RGBA => Self::RGBA,
-            PixelFormat::BGR => Self::BGR,
-            PixelFormat::BGRA => Self::BGRA,
+            Some(PixelFormat::RGB) => Self::RGB,
+            Some(PixelFormat::RGBA) => Self::RGBA,
+            Some(PixelFormat::BGR) => Self::BGR,
+            Some(PixelFormat::BGRA) => Self::BGRA,
+            None => Self::YCbCr,
             _ => Self::Unknown,
         }
     }
 }
 
-impl TryInto<PixelFormat> for ColorSpace {
+impl TryInto<Option<PixelFormat>> for ColorSpace {
     type Error = &'static str;
 
-    fn try_into(self) -> Result<PixelFormat, Self::Error> {
+    fn try_into(self) -> Result<Option<PixelFormat>, Self::Error> {
         match self {
-            ColorSpace::RGB => Ok(PixelFormat::RGB),
-            ColorSpace::RGBA => Ok(PixelFormat::RGBA),
-            ColorSpace::YCbCr => Err("YCbCr is not supported by TurboJpeg"),
-            ColorSpace::BGR => Ok(PixelFormat::BGR),
-            ColorSpace::BGRA => Ok(PixelFormat::BGRA),
+            ColorSpace::RGB => Ok(Some(PixelFormat::RGB)),
+            ColorSpace::RGBA => Ok(Some(PixelFormat::RGBA)),
+            ColorSpace::YCbCr => Ok(None),
+            ColorSpace::BGR => Ok(Some(PixelFormat::BGR)),
+            ColorSpace::BGRA => Ok(Some(PixelFormat::BGRA)),
             ColorSpace::Unknown => Err("Unknown is not supported by TurboJpeg"),
         }
     }
 }
 
 /// TurboJpeg color processor
-pub struct TurboColorProcessor(PixelFormat);
+pub struct TurboColorProcessor {
+    color_space: Option<PixelFormat>,
+}
 
 impl TurboColorProcessor {
     pub fn new(colorspace: ColorSpace) -> Result<Self, Box<dyn Error>> {
-        Ok(Self(colorspace.try_into()?))
+        Ok(Self {
+            color_space: colorspace.try_into()?,
+        })
     }
 }
 
 impl ProcessorTrait<ColorPacket, ColorFrame> for TurboColorProcessor {
     async fn process(&self, input: ColorPacket) -> Result<ColorFrame, Box<dyn Error>> {
-        let image = decompress(&input.jpeg_buffer, self.0)?;
+        let mut decompressor = Decompressor::new()?;
+        let header = decompressor.read_header(&input.jpeg_buffer)?;
 
-        Ok(ColorFrame {
-            color_space: image.format.into(),
-            width: image.width,
-            height: image.height,
-            buffer: image.pixels,
-            sequence: input.sequence,
-            timestamp: input.timestamp,
-            exposure: input.exposure,
-            gain: input.gain,
-            gamma: input.gamma,
-        })
-    }
+        if let Some(color_space) = self.color_space {
+            let pitch = header.width * color_space.size();
+            let mut image = Image {
+                pixels: vec![0; header.height * pitch],
+                width: header.width,
+                pitch,
+                height: header.height,
+                format: color_space,
+            };
 
-    fn pipe<'a, 'b, T, P>(
-        &'a self,
-        processor: &'b P,
-    ) -> crate::processor::PipedProcessor<'a, 'b, ColorPacket, ColorFrame, T, Self, P>
-    where
-        Self: Sized,
-        P: ProcessorTrait<ColorFrame, T>,
-    {
-        crate::processor::PipedProcessor {
-            _input: std::marker::PhantomData::default(),
-            _tmp: std::marker::PhantomData::default(),
-            _output: std::marker::PhantomData::default(),
-            processor1: self,
-            processor2: processor,
+            decompressor.decompress(&input.jpeg_buffer, image.as_deref_mut())?;
+
+            Ok(ColorFrame {
+                color_space: self.color_space.into(),
+                width: image.width,
+                height: image.height,
+                buffer: image.pixels,
+                sequence: input.sequence,
+                timestamp: input.timestamp,
+                exposure: input.exposure,
+                gain: input.gain,
+                gamma: input.gamma,
+            })
+        } else {
+            let align = 4;
+            let yuv_pixels_len =
+                yuv_pixels_len(header.width, align, header.height, header.subsamp)?;
+            let mut yuv_image = YuvImage {
+                pixels: vec![0; yuv_pixels_len],
+                width: header.width,
+                align,
+                height: header.height,
+                subsamp: header.subsamp,
+            };
+
+            decompressor.decompress_to_yuv(&input.jpeg_buffer, yuv_image.as_deref_mut())?;
+
+            Ok(ColorFrame {
+                color_space: self.color_space.into(),
+                width: yuv_image.width,
+                height: yuv_image.height,
+                buffer: yuv_image.pixels,
+                sequence: input.sequence,
+                timestamp: input.timestamp,
+                exposure: input.exposure,
+                gain: input.gain,
+                gamma: input.gamma,
+            })
         }
     }
 }
